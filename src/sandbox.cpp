501be9d2dc79555eb1cad354b94a3dc2ccb931e1
wanmen <wanmen@debian>
2019-08-14 09:13:33 +0800

    structure

---
+++ src/sandbox.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+class myBase {
+public:
+  myBase() {
+    cout << "myBase ctr" << endl;
+  }
+  ~myBase() {
+    cout << "myBase dtr" << endl;
+  }  
+};
+
+// class derivedOne : public myBase {
+class derivedOne : private myBase {
+public:
+  derivedOne() {
+    cout << "derOne ctr" << endl;    
+  }
+  ~derivedOne() {
+    cout << "derOne dtr" << endl;    
+  }  
+};
+
+int main() {
+
+  derivedOne derOneTester {};
+
+  myBase* myBaseTester = &derOneTester;
+
+  (void)myBaseTester;
+  return 0;
+}
